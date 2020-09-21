0c28c6cb582b8754ddc4c592af945a42001d594c
Paulo Jose Ribeiro Silva <paulo.jose@mrv.com.br>
2020-09-21 00:21:38 -0300

    1) Adding Robots page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ const Index = () => (
     <div>
         <h1>SSR Magician</h1>
         <Link href='/about'><button>About</button></Link>
+        <Link href='/Robots'><button>Robots</button></Link>
         {/* <a href='./about'>About</a> */}
     </div>
 
